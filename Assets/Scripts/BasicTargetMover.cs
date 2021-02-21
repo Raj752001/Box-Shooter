c9cacaf33741f4e2efb664f08b4e78abed99d6b4
Raj752001 <u19cs011@coed.svnit.ac.in>
2021-02-21 11:33:02 +0530

    1st commit

---
+++ Assets/Scripts/BasicTargetMover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTargetMover : MonoBehaviour {
+
+    public float spinSpeed = 180f;
+    public float motionMagnitude = 0.1f;
+
+    public bool doSpin = true;
+    public bool doMotion = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (doSpin)
+        {
+            // Rotate around 'UP' axix
+            gameObject.transform.Rotate(Vector3.up * spinSpeed * Time.deltaTime);
+        }
+
+        if (doMotion)
+        {
+            // Move up and down
+            gameObject.transform.Translate(Vector3.up * motionMagnitude * Mathf.Cos(Time.timeSinceLevelLoad));
+        }
+        
+    }
+}
